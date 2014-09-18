7937e4861fa8fecce4e79f8a1e1b5fd67621c64f
Daniel Zimmermann <daniel.luke.zimmermann@gmail.com>
2014-09-18 11:51:01 +1000

    chore(Gruntfile): include grunt-bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,6 +96,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-conventional-changelog');
   grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-karma');
